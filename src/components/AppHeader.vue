5cbc6f438d5394b948d1e975428e6d0cd176f756
xrafffcode <yusfiwawan2@gmail.com>
2024-06-11 19:31:10 +0700

    member login

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,3 +1,40 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+import { storeToRefs } from 'pinia'
+import { RouterLink } from 'vue-router'
+import { ref, onMounted } from 'vue'
+
+const { loading, error, user } = storeToRefs(useAuthStore())
+const { login } = useAuthStore()
+
+const showLoginModal = ref(false)
+
+const form = ref({
+  email: '',
+  password: '',
+})
+
+const onSubmit = () => {
+    login(form.value)
+
+    if (!error.value) {
+        showLoginModal.value = false
+    }
+
+    form.value = {
+        email: '',
+        password: '',
+    }
+
+    error.value = null
+}
+
+onMounted(() => {
+    error.value = null
+    loading.value = false
+})
+</script>
+
 <template>
     <header>
         <div class="main_header">
@@ -5,14 +42,16 @@
                 <nav class="navbar navbar-expand-lg navbar-light p-0">
                     <a class="navbar-brand" href="./index.html">
                         <figure class="mb-0">
-                            <img src="@/assets/images/logo.png" alt="Fit & Fun" >
+                            <img src="@/assets/images/logo.png" alt="Fit & Fun">
                         </figure>
                     </a>
-                    
-                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    <span class="navbar-toggler-icon"></span>
-                    <span class="navbar-toggler-icon"></span>
+
+                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
                     </button>
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -33,7 +72,10 @@
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link contact_us" href="./contact.html">Login</a>
+                                {{ user?.profile?.name }}
+                                <a class="nav-link contact_us" href="#" @click="showLoginModal = true" v-if="!user">
+                                    Login
+                                </a>
                             </li>
                         </ul>
                     </div>
@@ -41,4 +83,113 @@
             </div>
         </div>
     </header>
+
+    <div class="login_modal" v-if="showLoginModal">
+        <div class="login_modal_content">
+            <div class="login_modal_close">
+                <a href="#" @click="showLoginModal = false">
+                    <i class="fas fa-times"></i>
+                </a>
+            </div>
+            <div class="login_modal_body">
+                <div class="login_modal_body_inner">
+                    <h3>Login Member</h3>
+                    <div class="login_modal_error" v-if="error && !error.email && !error.password">
+                        {{ error }}
+                    </div>
+                    <div class="login_modal_form">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Email" v-model="form.email" :class="{ 'is-invalid': error && error.email }">
+                    
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Password" v-model="form.password" :class="{ 'is-invalid': error && error.password }">    
+                        </div>
+                        <div class="form-group">
+                            <a href="#" class="login_modal_btn" @click="onSubmit">
+                                Login
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.login_modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.3s ease-in-out;
+}
+
+.login_modal_content {
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    max-width: 400px;
+    width: 100%;
+}
+
+.login_modal_close {
+    text-align: right;
+}
+
+.login_modal_close a {
+    font-size: 20px;
+    color: #eae2d3;
+}
+
+.login_modal_body {
+    text-align: center;
+}
+
+.login_modal_body_inner {
+    max-width: 500px;
+    margin: 0 auto;
+}
+
+.login_modal_body_inner h3 {
+    font-size: 24px;
+    margin-bottom: 10px;
+}
+
+.login_modal_body_inner p {
+    font-size: 16px;
+    margin-bottom: 20px;
+}
+
+.login_modal_form {
+    max-width: 400px;
+    margin: 0 auto;
+}
+
+.login_modal_form .form-group {
+    margin-bottom: 20px;
+}
+
+.login_modal_btn {
+    display: inline-block;
+    background-color: #eae2d3;
+    color: #000;
+    padding: 10px 20px;
+    border-radius: 5px;
+    text-decoration: none;
+    width: 100%;
+}
+
+.login_modal_error {
+    color: red;
+    font-size: 14px;
+    margin-bottom: 10px;
+}
+</style>
---
+++ src/stores/auth.js
@@ -0,0 +1,82 @@
+import { axiosInstance } from '@/plugins/axios'
+import router from '@/router'
+import { defineStore } from 'pinia'
+import { handleError } from '@/helpers/errorHelper'
+
+export const useAuthStore = defineStore({
+  id: 'auth',
+  state: () => ({
+    user: null,
+    loading: false,
+    error: null,
+    success: null,
+  }),
+  getters: {
+    token: state => localStorage.getItem('token'),
+  },
+  actions: {
+    async login(credentials) {
+      this.loading = true
+
+      try {
+        const response = await axiosInstance.post('/login', credentials)
+
+        const token = response.data.token
+
+        localStorage.setItem('token', token)
+
+        this.success = 'Login successful'
+
+        this.checkAuth()
+        
+        window.location.reload()
+      } catch (error) {
+        this.error = handleError(error)
+      } finally {
+        this.loading = false
+      }
+    },
+    async logout() {
+      this.loading = true
+
+      try {
+        await axiosInstance.post('/logout')
+
+        localStorage.removeItem('token')
+
+        this.user = null
+        this.error = null
+        this.success = 'Logout successful'
+
+        router.push({ name: 'login' })
+      } catch (error) {
+        this.error = handleError(error)
+
+        localStorage.removeItem('token')
+
+        this.user = null
+        this.success = null
+
+        router.push({ name: 'login' })
+      } finally {
+        this.loading = false
+      }
+    },
+    async checkAuth() {
+      this.loading = true
+
+      try {
+        const response = await axiosInstance.get('/me')
+
+        this.user = response.data.data
+
+        return this.user
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          this.logout()
+        }
+      }
+      this.loading = false
+    },
+  },
+})
